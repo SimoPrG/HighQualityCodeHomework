3a361dfefa291668f7fa2306698ed2e54e69b2c4
SimoPrG <simeon_petev@hotmail.com>
2015-07-06 00:16:06 +0300

    Variables Data Expressions And Constants

---
+++ VariablesDataExpressionsAndConstants/Figure/Size.cs
@@ -0,0 +1,63 @@
+﻿namespace Figure
+{
+    using System;
+
+    /// <summary>
+    /// Contains figure measures and helper methods to work with this measures.
+    /// </summary>
+    public class Size
+    {
+        /// <summary>
+        /// Contains the width of the figure.
+        /// </summary>
+        private double width; 
+
+        /// <summary>
+        /// Contains the height of the figure.
+        /// </summary>
+        private double height;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Size"/> class.
+        /// </summary>
+        /// <param name="width">The width of the new figure.</param>
+        /// <param name="height">The height of the new figure.</param>
+        public Size(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        /// <summary>
+        /// Gets or sets the width size.
+        /// </summary>
+        public double Width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the height size.
+        /// </summary>
+        public double Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+
+        /// <summary>
+        /// Gets the rotated size of <paramref name="size"/>.
+        /// </summary>
+        /// <param name="size">The figure size to get the measures from.</param>
+        /// <param name="rotationAngle">Rotation angle in radians.</param>
+        /// <returns>A new instance of the <see cref="Size"/> class with the measures after rotation.</returns>
+        public static Size GetRotatedSize(Size size, double rotationAngle)
+        {
+            double width = (Math.Abs(Math.Cos(rotationAngle)) * size.Width) + (Math.Abs(Math.Sin(rotationAngle)) * size.Height);
+            double height = (Math.Abs(Math.Sin(rotationAngle)) * size.Width) + (Math.Abs(Math.Cos(rotationAngle)) * size.Height);
+
+            return new Size(width, height);
+        }
+    }
+}
---
+++ VariablesDataExpressionsAndConstants/Statistics/Printer.cs
@@ -0,0 +1,80 @@
+﻿namespace Statistics
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Provides static methods to print collection statistics.
+    /// </summary>
+    public static class Printer
+    {
+        /// <summary>
+        /// Prints the minimum value found in the <paramref name="values"/> up to <paramref name="position"/> on the console.
+        /// </summary>
+        /// <param name="values">The collection of values.</param>
+        /// <param name="position">The position to which to look for the minimum value.</param>
+        public static void PrintMin(IList<double> values, int position)
+        {
+            double minValue = values[0];
+            for (int i = 1; i < position; i++)
+            {
+                if (values[i] < minValue)
+                {
+                    minValue = values[i];
+                }
+            }
+
+            Console.WriteLine(minValue);
+        }
+
+        /// <summary>
+        /// Prints the maximum value found in the <paramref name="values"/> up to <paramref name="position"/> on the console.
+        /// </summary>
+        /// <param name="values">The collection of values.</param>
+        /// <param name="position">The position to which to look for the maximum value.</param>
+        public static void PrintMax(IList<double> values, int position)
+        {
+            double maxValue = values[0];
+            for (int i = 1; i < position; i++)
+            {
+                if (maxValue < values[i])
+                {
+                    maxValue = values[i];
+                }
+            }
+
+            Console.WriteLine(maxValue);
+        }
+
+        /// <summary>
+        /// Prints the average value of the <paramref name="values"/> up to <paramref name="position"/> on the console.
+        /// </summary>
+        /// <param name="values">The collection of values.</param>
+        /// <param name="position">The position to which to calculate the average value.</param>
+        public static void PrintAverage(IList<double> values, int position)
+        {
+            double sum = 0;
+            for (int i = 0; i < position; i++)
+            {
+                sum += values[i];
+            }
+
+            double average = sum / position;
+            Console.WriteLine(average);
+        }
+
+        /// <summary>
+        /// Prints the statistics of <paramref name="values"/> up to <paramref name="position"/> on the console.
+        /// </summary>
+        /// <param name="values">The collection of values.</param>
+        /// <param name="position">The position to which to calculate the statistics.</param>
+        public static void PrintStatistics(IList<double> values, int position)
+        {
+            PrintMax(values, position);
+
+            PrintMin(values, position);
+
+            PrintAverage(values, position);
+        }
+    }
+}
