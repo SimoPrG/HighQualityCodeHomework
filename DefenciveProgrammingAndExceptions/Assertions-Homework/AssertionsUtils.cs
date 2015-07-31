f30c2a320a2f143d8930f573f5e12c2b704e7f84
SimoPrG <simeon_petev@hotmail.com>
2015-07-31 16:37:25 +0300

    Assertions

---
+++ DefenciveProgrammingAndExceptions/Assertions-Homework/AssertionsUtils.cs
@@ -0,0 +1,96 @@
+﻿namespace Assertions
+{
+    using System;
+    using System.Diagnostics;
+
+    public static class AssertionsUtils
+    {
+        public static void SelectionSort<T>(T[] arr) where T : IComparable<T>
+        {
+            Debug.Assert(arr != null, "Array is null.");
+            Debug.Assert(arr.Length > 0, "Array is empty.");
+
+            int length = arr.Length;
+            for (int index = 0; index < arr.Length - 1; index++)
+            {
+                int minElementIndex = FindMinElementIndex(arr, index, arr.Length - 1);
+                Swap(ref arr[index], ref arr[minElementIndex]);
+            }
+
+            Debug.Assert(arr.Length == length, "Input and output arrays have different lengths.");
+        }
+
+        public static int BinarySearch<T>(T[] arr, T value) where T : IComparable<T>
+        {
+            return BinarySearch(arr, value, 0, arr.Length - 1);
+        }
+
+        private static int BinarySearch<T>(T[] arr, T value, int startIndex, int endIndex)
+            where T : IComparable<T>
+        {
+            Debug.Assert(arr != null, "Array is null.");
+            Debug.Assert(arr.Length > 0, "Array is empty.");
+            Debug.Assert(value != null, "Searching value is null.");
+            Debug.Assert(startIndex >= 0 && startIndex <= arr.Length - 1, "Invalid startIndex value.");
+            Debug.Assert(endIndex >= 0 && endIndex <= arr.Length - 1, "Invalid endIndex value.");
+            Debug.Assert(startIndex <= endIndex, "Invalid start- and endIndex values.");
+
+            while (startIndex <= endIndex)
+            {
+                int midIndex = (startIndex + endIndex) / 2;
+                if (arr[midIndex].Equals(value))
+                {
+                    return midIndex;
+                }
+
+                if (arr[midIndex].CompareTo(value) < 0)
+                {
+                    // Search on the right half
+                    startIndex = midIndex + 1;
+                }
+                else
+                {
+                    // Search on the right half
+                    endIndex = midIndex - 1;
+                }
+            }
+
+            Debug.Assert(startIndex > endIndex, "Searching algorithm logic is error-prone.");
+
+            // Searched value not found
+            return -1;
+        }
+
+        private static void Swap<T>(ref T x, ref T y)
+        {
+            Debug.Assert(x != null, "First parameter is null.");
+            Debug.Assert(y != null, "Second parameter is null.");
+            T oldX = x;
+            x = y;
+            y = oldX;
+        }
+
+        private static int FindMinElementIndex<T>(T[] arr, int startIndex, int endIndex)
+    where T : IComparable<T>
+        {
+            Debug.Assert(arr != null, "Array is null.");
+            Debug.Assert(arr.Length > 0, "Array is empty.");
+            Debug.Assert(startIndex >= 0 && startIndex <= arr.Length - 1, "Invalid startIndex value.");
+            Debug.Assert(endIndex >= 0 && endIndex <= arr.Length - 1, "Invalid endIndex value.");
+            Debug.Assert(startIndex <= endIndex, "Invalid start and endIndex values.");
+
+            int minElementIndex = startIndex;
+            for (int i = startIndex + 1; i <= endIndex; i++)
+            {
+                if (arr[i].CompareTo(arr[minElementIndex]) < 0)
+                {
+                    minElementIndex = i;
+                }
+            }
+
+            Debug.Assert(minElementIndex >= 0 && minElementIndex <= arr.Length - 1, "Invalid minElementIndex value.");
+
+            return minElementIndex;
+        }
+    }
+}
---
+++ DefenciveProgrammingAndExceptions/Assertions-Homework/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assertions
+{
+    using System;
+
+    public static class Program
+    {
+        static void Main()
+        {
+            int[] arr = new int[] { 3, -1, 15, 4, 17, 2, 33, 0 };
+            Console.WriteLine("arr = [{0}]", string.Join(", ", arr));
+            AssertionsUtils.SelectionSort(arr);
+            Console.WriteLine("sorted = [{0}]", string.Join(", ", arr));
+
+            AssertionsUtils.SelectionSort(new int[0]); // Test sorting empty array
+            AssertionsUtils.SelectionSort(new int[1]); // Test sorting single element array
+
+            Console.WriteLine(AssertionsUtils.BinarySearch(arr, -1000));
+            Console.WriteLine(AssertionsUtils.BinarySearch(arr, 0));
+            Console.WriteLine(AssertionsUtils.BinarySearch(arr, 17));
+            Console.WriteLine(AssertionsUtils.BinarySearch(arr, 10));
+            Console.WriteLine(AssertionsUtils.BinarySearch(arr, 1000));
+        }
+    }
+}
