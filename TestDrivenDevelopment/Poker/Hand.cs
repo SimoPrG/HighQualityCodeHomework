77724f538675d5f15309635d511f88d3a2d7b561
SimoPrG <simeon_petev@hotmail.com>
2015-08-09 22:21:58 +0300

    Test Driven Development

---
+++ TestDrivenDevelopment/Poker/Hand.cs
@@ -0,0 +1,44 @@
+﻿namespace Poker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Hand : IHand
+    {
+        public Hand(IList<ICard> cards)
+        {
+            if (cards == null)
+            {
+                throw new ArgumentNullException("cards", "List of cards cannot be null.");
+            }
+
+            if (cards.Any(c => c == null))
+            {
+                throw new ArgumentNullException("cards", "Any card in cards cannot be null");
+            }
+
+            this.Cards = cards;
+        }
+
+        public enum Type
+        {
+            HighCard = 0,
+            OnePair = 1,
+            TwoPair = 2,
+            ThreeOfAKind = 3,
+            Straight = 4,
+            Flush = 5,
+            FullHouse = 6,
+            FourOfAKind = 7,
+            StraightFlush = 8
+        }
+
+        public IList<ICard> Cards { get; private set; }
+
+        public override string ToString()
+        {
+            return string.Join(" | ", this.Cards);
+        }
+    }
+}
