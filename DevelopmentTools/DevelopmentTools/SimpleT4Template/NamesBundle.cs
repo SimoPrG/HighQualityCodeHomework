477171ed07b3f3f1ecc15a1afddc79b0c8bc7a4a
SimoPrG <simeon_petev@hotmail.com>
2015-08-18 17:41:25 +0300

    Development Tools

---
+++ DevelopmentTools/DevelopmentTools/SimpleT4Template/NamesBundle.cs
@@ -0,0 +1,161 @@
+﻿namespace SimpleT4Template
+{
+	using System.Collections.Generic;
+
+	public class NamesBundle
+	{
+		public string Name1 { get; set; }
+
+		public string Name2 { get; set; }
+
+		public string Name3 { get; set; }
+
+		public string Name4 { get; set; }
+
+		public string Name5 { get; set; }
+
+		public string Name6 { get; set; }
+
+		public string Name7 { get; set; }
+
+		public string Name8 { get; set; }
+
+		public string Name9 { get; set; }
+
+		public string Name10 { get; set; }
+
+		public string Name11 { get; set; }
+
+		public string Name12 { get; set; }
+
+		public string Name13 { get; set; }
+
+		public string Name14 { get; set; }
+
+		public string Name15 { get; set; }
+
+		public string Name16 { get; set; }
+
+		public string Name17 { get; set; }
+
+		public string Name18 { get; set; }
+
+		public string Name19 { get; set; }
+
+		public string Name20 { get; set; }
+
+		public string Name21 { get; set; }
+
+		public string Name22 { get; set; }
+
+		public string Name23 { get; set; }
+
+		public string Name24 { get; set; }
+
+		public string Name25 { get; set; }
+
+		public string Name26 { get; set; }
+
+		public string Name27 { get; set; }
+
+		public string Name28 { get; set; }
+
+		public string Name29 { get; set; }
+
+		public string Name30 { get; set; }
+
+		public string Name31 { get; set; }
+
+		public string Name32 { get; set; }
+
+		public string Name33 { get; set; }
+
+		public string Name34 { get; set; }
+
+		public string Name35 { get; set; }
+
+		public string Name36 { get; set; }
+
+		public string Name37 { get; set; }
+
+		public string Name38 { get; set; }
+
+		public string Name39 { get; set; }
+
+		public string Name40 { get; set; }
+
+		public string Name41 { get; set; }
+
+		public string Name42 { get; set; }
+
+		public string Name43 { get; set; }
+
+		public string Name44 { get; set; }
+
+		public string Name45 { get; set; }
+
+		public string Name46 { get; set; }
+
+		public string Name47 { get; set; }
+
+		public string Name48 { get; set; }
+
+		public string Name49 { get; set; }
+
+		public string Name50 { get; set; }
+
+		public IEnumerable<string> GetProperties()
+		{
+			yield return this.Name1;
+			yield return this.Name2;
+			yield return this.Name3;
+			yield return this.Name4;
+			yield return this.Name5;
+			yield return this.Name6;
+			yield return this.Name7;
+			yield return this.Name8;
+			yield return this.Name9;
+			yield return this.Name10;
+			yield return this.Name11;
+			yield return this.Name12;
+			yield return this.Name13;
+			yield return this.Name14;
+			yield return this.Name15;
+			yield return this.Name16;
+			yield return this.Name17;
+			yield return this.Name18;
+			yield return this.Name19;
+			yield return this.Name20;
+			yield return this.Name21;
+			yield return this.Name22;
+			yield return this.Name23;
+			yield return this.Name24;
+			yield return this.Name25;
+			yield return this.Name26;
+			yield return this.Name27;
+			yield return this.Name28;
+			yield return this.Name29;
+			yield return this.Name30;
+			yield return this.Name31;
+			yield return this.Name32;
+			yield return this.Name33;
+			yield return this.Name34;
+			yield return this.Name35;
+			yield return this.Name36;
+			yield return this.Name37;
+			yield return this.Name38;
+			yield return this.Name39;
+			yield return this.Name40;
+			yield return this.Name41;
+			yield return this.Name42;
+			yield return this.Name43;
+			yield return this.Name44;
+			yield return this.Name45;
+			yield return this.Name46;
+			yield return this.Name47;
+			yield return this.Name48;
+			yield return this.Name49;
+			yield return this.Name50;
+		}
+	}
+}
---
+++ DevelopmentTools/DevelopmentTools/SimpleT4Template/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleT4Template
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var namesBundle = new NamesBundle();
+            namesBundle.Name1 = "Simeon";
+            namesBundle.Name25 = "Pesho";
+            namesBundle.Name50 = "Stamat";
+
+            foreach (var name in namesBundle.GetProperties())
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
